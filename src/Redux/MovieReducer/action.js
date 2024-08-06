768d30a883ce21e5ff894391001adb28673a7af5
Ankita <ankitakumawat111@gmail.com>
2024-08-06 14:01:30 +0530

    fix error

--- src/Redux/MovieReducer/action.js
+++ src/Redux/MovieReducer/action.js
@@ -1,13 +1,10 @@
+import axios from 'axios';
 import { FETCH_MOVIES_REQUEST, FETCH_MOVIES_SUCCESS, FETCH_MOVIES_FAILURE } from './actionTypes';
-
 
 export const fetchMovies = () => async (dispatch) => {
     dispatch({ type: FETCH_MOVIES_REQUEST });
     try {
-
-        const asyncfunction = await fetch(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/movies`)
-        const data = await asyncfunction.json();
-        const response = { data };
+        const response = await axios.get(`http://localhost:${process.env.REACT_APP_JSON_SERVER_PORT}/movies`);
         dispatch({ type: FETCH_MOVIES_SUCCESS, payload: response.data });
     } catch (error) {
         dispatch({ type: FETCH_MOVIES_FAILURE, payload: error.message });
